2f63a5ed7a9cb46bcb6ecc0d006dc33346484e68
Nina <nsd286@nyu.edu>
2025-11-15 10:23:30 -0500

    set up InteractionManager to detect clicks and cause avoidance within flock

---
+++ Assets/Scripts/Runtime/InteractionManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _touchObstaclePrefab;
+    private GameObject _touchObstacleRef;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10f;
+
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            if (_touchObstacleRef != null)
+            {
+                _touchObstacleRef.SetActive(true);
+                _touchObstacleRef.transform.position = worldPos;
+            }
+            else
+            {
+                GameObject go = Instantiate(_touchObstaclePrefab, worldPos, Quaternion.identity);
+                _touchObstacleRef = go;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(_touchObstacleRef != null)
+            {
+                _touchObstacleRef.SetActive(false);
+            }
+        }
+    }
+}
